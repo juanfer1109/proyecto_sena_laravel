5157cd3f48adfa0f701e5563ada6de33936141d5
juanfer1109 <juanfer_arango@hotmail.com>
2024-06-02 10:52:10 -0500

    First Commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+import * as bootstrap from 'bootstrap';
+
+let linkList = document.getElementById('linkList')
+
+function miMenu() {
+  let x = document.getElementById("myTopnav")
+  if (x.className === "topnav") {
+    x.className += " responsive"
+  } else {
+    x.className = "topnav"
+  }
+}
+
+linkList.addEventListener('click', function(){
+  miMenu()
+})
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\EquipoController;
+use App\Http\Controllers\TipoController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/equipos/home', function () {
+    return view('equipos.menu');
+});
+
+Route::get('/users/home', function () {
+    return view('users.menu');
+});
+
+Route::resource('/equipos', EquipoController::class);
+
+Route::resource('/tipos', TipoController::class);
+
+Route::resource('/rols', RolController::class);
+
+Route::resource('/users', UserController::class);
